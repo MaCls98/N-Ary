f1a87b3446628c0be9b24bd35b5f23838a4a3ca1
MaCls98 <manuel.celis@uptc.edu.co>
2018-04-12 01:08:07 -0500

    Creacion del paquete de la vista y la clase FileChooser, encargada de seleccionar la carpeta que se desea analizar

---
+++ src/view/FileChooser.java
@@ -0,0 +1,18 @@
+package view;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+public class FileChooser extends JFileChooser{
+	
+	private static final long serialVersionUID = 1L;
+
+	public File chooseFolder(){
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		chooser.showOpenDialog(getRootPane());
+		return chooser.getSelectedFile();
+	}
+	
+}
