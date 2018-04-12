6e8f0c3682025cb1edad391eba67589b83c65ef6
MaCls98 <manuel.celis@uptc.edu.co>
2018-04-12 00:08:03 -0500

    Creacion del arbol n-ario junto con sus atributos

---
+++ src/model/TreeFiles.java
@@ -0,0 +1,19 @@
+package model;
+
+public class TreeFiles {
+	
+	private Node root;
+	private int totalSize;
+	public Node getRoot() {
+		return root;
+	}
+	public void setRoot(Node root) {
+		this.root = root;
+	}
+	public int getTotalSize() {
+		return totalSize;
+	}
+	public void setTotalSize(int totalSize) {
+		this.totalSize = totalSize;
+	}
+}
